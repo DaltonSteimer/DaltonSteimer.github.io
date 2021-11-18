04b7f4ddd447b420ebc2b920da2239e7034ec92d
Dalton Steimer <94022462+DaltonSteimer@users.noreply.github.com>
2021-11-18 10:28:45 -0500

    Update viewport.js

--- viewport.js
+++ viewport.js
@@ -1,11 +1,23 @@
-<script>
-function openTab(evt, TabName)
-{var i, tabcontent, tablinks;
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) 
-  {tabcontent[i].style.display = "none";}
-  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");}
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";}
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
