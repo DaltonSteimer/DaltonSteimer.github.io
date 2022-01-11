972ce616be2ad1ffac124f21b2657ae07bb81530
Dalton Steimer <94022462+DaltonSteimer@users.noreply.github.com>
2022-01-11 16:00:22 -0500

    Update newproject.java

--- newproject.java
+++ newproject.java
@@ -5,7 +5,7 @@ import java.io.IOException;
  
 public class WriteToFileExample {
   public static void main(String[] args) {
-                try {
+                {
                  
                       system.out.println("hi")
                  }
