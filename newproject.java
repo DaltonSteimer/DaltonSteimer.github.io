2195d77f336f84fbad2e9526719046eb69e92a3c
Dalton Steimer <94022462+DaltonSteimer@users.noreply.github.com>
2022-01-11 15:57:35 -0500

    Update and rename java code to newproject.java

---
+++ newproject.java
@@ -0,0 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+ 
+public class WriteToFileExample {
+  public static void main(String[] args) {
+                try {
+                 
+                      system.out.println("hi")
+                 }
+  }
+}
